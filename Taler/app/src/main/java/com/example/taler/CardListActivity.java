140f5cf0840980c67cf408925d3874f6f124ffc4
Yee-Kun Jung <48750313+Twoketchupplz@users.noreply.github.com>
2020-11-11 20:53:19 +0900

    Cardlist UI
    Recycler View

--- Taler/app/src/main/java/com/example/taler/CardListActivity.java
+++ Taler/app/src/main/java/com/example/taler/CardListActivity.java
@@ -1,6 +1,8 @@
 package com.example.taler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,6 +15,16 @@ public class CardListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_list);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true); // 모은 아이템뷰의 사이즈가 고정되어있음
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this); //layoutManager 생성
+        recyclerView.setLayoutManager(layoutManager); //연결
+
+        RecyclerView.Adapter adapter = new CardListRecyclerViewAdapter(); // 객체생성
+        recyclerView.setAdapter(adapter);
+
 
         Button selectButton = findViewById(R.id.btn_select);
         selectButton.setOnClickListener(new View.OnClickListener() {
